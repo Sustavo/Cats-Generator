7a78959fec0dfe255b441d3ba4ebaba816026045
Gustavo <gust4v0309@gmail.com>
2023-03-09 19:09:40 -0300

    Style: ButtonFrog

--- src/components/ButtonFrog.vue
+++ src/components/ButtonFrog.vue
@@ -30,5 +30,53 @@ export default {
 </script>
 
 <style scoped>
+button {
+    background: #F0D3D3;
+    border: none;
+    width: 322px;
+    height: 113px;
+    border-radius: 50px;
+    font-size: 34px;
+    padding: 22px 65px;
+    cursor: pointer;
+
+}
+
+span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 60px;
+    border-radius: 12rem 12rem 0 0 ;
+    background: #F5DEDE;
+    cursor: pointer;
+
+;
+}
+
+.main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 322px;
+}
+
+.main:hover {
+    transform: scale(1.05);
+}
+
+.frog-eyes {
+    display: flex;
+    gap: 1.6875rem;
+
+}
+
+.eye {
+    width: 44.5px;
+    height: 32px;
+    border-radius: 12rem 12rem 0 0 ;
+    background: #D9D9D9;
+}
 
 </style>
