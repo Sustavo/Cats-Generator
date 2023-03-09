168836e95ba7cdaab8ddd3b12e184ae5ff005465
Gustavo <gust4v0309@gmail.com>
2023-03-09 19:08:57 -0300

    Feat: Create ButtonAbout

---
+++ src/components/ButtonAbout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <button @click="handleClick">{{ buttonText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ButtonAbout",
+    props: {
+        buttonText: {
+            type: String,
+            default: 'about me'
+        }
+    },
+    methods: {
+        handleClick() {
+            this.$emit("button-clicked")
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
