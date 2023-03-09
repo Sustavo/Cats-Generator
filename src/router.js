762b533297007bb66a35b494a707b7df9b94ac9a
Gustavo <gust4v0309@gmail.com>
2023-03-09 19:02:48 -0300

    Feat: create router.js and make a route to "home"

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/HomeView.vue"
+import About from "./views/AboutView.vue"
+import CatGenerator from "./views/CatGeneratorView.vue"
+
+Vue.use(Router);
+
+const router = new Router({
+
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home
+        },
+
+
+    ]
+    
+});
+
+export default router;
