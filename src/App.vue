1098eff02039cd2ffa3febd1640404ad9e590df3
Gustavo <gust4v0309@gmail.com>
2023-03-09 19:02:15 -0300

    Feat: create a main and use router-view to use routes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <main>
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <style>
+body {
+  background: #C0FBF4;
+}
 
 </style>
