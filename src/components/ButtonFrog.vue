39b7abd1bfc98f2d2cae4c618f05b00426e3059c
Gustavo <gust4v0309@gmail.com>
2023-03-09 19:09:26 -0300

    Feat: create ButtonFrog

---
+++ src/components/ButtonFrog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main">
+    <div class="frog-eyes">
+        <span>
+            <div class="eye"></div>
+        </span>
+        <span>
+            <div class="eye"></div>
+        </span>
+    </div>
+    <button @click="handleClick" >{{ buttonText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ButtonFrog",
+    props: {
+        buttonText: {
+            type: String,
+            default: "tap here to see cats"
+        },
+    },
+    methods: {
+        handleClick() {
+            this.$emit("button-clicked");
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
