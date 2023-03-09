706c993d6382934afeb08a29b17beb28094a5f24
Gustavo <gust4v0309@gmail.com>
2023-03-09 20:32:27 -0300

    Feat: Create animation to the "p" in AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p class="animate-text typing-animation">Hi, I like cats :)</p>
   </div>
 </template>
 
@@ -10,6 +10,52 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.animate-text {
+  font-size: 50px;
+  white-space: nowrap;
+  overflow: hidden;
+  width: 0;
+  max-width: 320px;
+  animation: typing 2s steps(40) 1;
+  position: relative;
+  font-size: 50px;
+}
 
+.animate-text::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 0.15em;
+  border-right: 0.05em solid rgb(119, 119, 119);
+  animation: blink 1s infinite;
+}
+
+@keyframes typing {
+    from {
+        width: 0;
+    }
+    to {
+        width: 100%;
+    }
+}
+
+@keyframes blink {
+    50% {
+        border-color: transparent;
+    }
+}
+
+.typing-animation {
+  animation: typing 12s steps(40) both;
+}
+
+.about {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
 </style>
