ae9cf5d6578cb74f15ef4b9f3e0283f803a08ee4
Gustavo <gust4v0309@gmail.com>
2023-03-09 19:09:10 -0300

    Style: ButtonAbout

--- src/components/ButtonAbout.vue
+++ src/components/ButtonAbout.vue
@@ -22,5 +22,18 @@ export default {
 </script>
 
 <style scoped>
+button {
+    background: #D9D9D9;
+    width: 171px;
+    height: 55px;
+    cursor: pointer;
+    border: none;
+    border-radius: 40px;
+    font-size: 28px;
+}
+
+button:hover {
+    transform: scale(1.05);
+}
 
 </style>
