a1f9f96ad705ee3c18a3389d35e9e432c40f8ba8
Gustavo <gust4v0309@gmail.com>
2023-03-09 23:45:36 -0300

    Feat: Create and make TheHeader

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="main">
+    <img @click="handleClick" src="../assets/cat-logo.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TheHeader",
+    methods: {
+        handleClick() {
+            if(this.$route.path !== "/") {
+                this.$router.push("/");
+            }
+        }
+    }
+
+
+}
+</script>
+
+<style scoped>
+.main {
+    display: flex;
+    align-items: center;
+    height: 80px;
+    background: #6EB5FF;
+}
+
+img {
+    width: 120px;
+    cursor: pointer;
+    margin-bottom: 10px;
+}
+
+img:hover {
+    transform: scale(1.10);
+}
+</style>
