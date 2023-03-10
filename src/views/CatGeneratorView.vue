285b05dc4933065d3777199310860691c8a11d72
Gustavo <gust4v0309@gmail.com>
2023-03-10 00:31:28 -0300

    Feat: testing Style CatGenerator to use the api

--- src/views/CatGeneratorView.vue
+++ src/views/CatGeneratorView.vue
@@ -1,6 +1,55 @@
 <template>
-  <div>
-    <img src="" alt="">
+  <div class="main">
+    <ul>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+      <li>
+        <img src="../assets/5954b408c66525ad932faa693a647e3f.jpg" alt="">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,6 +59,26 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .main {
+    display: flex;
+    margin: 15vh 0;
+    max-height: calc(100vh - 220px); 
+    overflow-y: auto; 
+  }
+
+  img {
+    width: 382px;
+    height: 260px;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
 
 </style>
