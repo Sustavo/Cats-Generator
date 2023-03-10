d6bc929a2979cbdcdb9013a055ce7c6086fa63fc
Gustavo <gust4v0309@gmail.com>
2023-03-09 22:57:08 -0300

    Feat: Create TheFooter and add components

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main">
+    <ul>
+        <li>
+            <a target="_blank" href="mailto:gust4v0309@gmail.com"><UilEnvelope size="50px" /></a>
+        </li>
+        <li>
+            <a target="_blank" href="https://www.linkedin.com/in/gustavos-castro/"><UilLinkedin size="50px" /></a>
+        </li>
+        <li>
+            <a target="_blank" href="https://github.com/Sustavo"><UilGithub size="50px" /></a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { UilEnvelope, UilLinkedin, UilGithub } from '@iconscout/vue-unicons'
+
+export default {
+    components: {
+        UilEnvelope,
+        UilLinkedin,
+        UilGithub
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
