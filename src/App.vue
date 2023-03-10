cdc677ed40bd56afef2ebf3663fd359498d64bef
Gustavo <gust4v0309@gmail.com>
2023-03-09 22:58:22 -0300

    Feat: add component "TheFooter"

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,29 @@
     <main>
       <router-view/>
     </main>
+    <footer>
+      <TheFooter />
+    </footer>
   </div>
 </template>
 
+<script>
+  import TheFooter from "./components/TheFooter.vue"
+
+export default {
+  components: {
+    TheFooter
+  }
+}
+</script>
 <style>
 body {
   background: #C0FBF4;
+  margin: 0;
+}
+
+main {
+  height: 100vh;
 }
 
 </style>
